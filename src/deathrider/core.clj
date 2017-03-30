2a600f4066ea52aeb08722dd02ed074305b34c72
Nick Ballard <znhihgiasy@gmail.com>
2017-03-30 14:48:21 +0800

    wip

--- src/deathrider/core.clj
+++ src/deathrider/core.clj
@@ -1,13 +1,7 @@
 (ns deathrider.core
   (:gen-class)
-  (:use [deathrider gameboard player point]))
+  (:use [deathrider server]))
 
 (defn -main
   [& args]
-  (let [gb (new-gameboard
-             [(new-player 1 (new-point -10 0) :right)
-              (new-player 2 (new-point 10 0) :left)])
-        gb (nth (iterate #(step % {}) gb) 5)
-        gb (step gb {1 :up})
-        gb (nth (iterate #(step % {}) gb) 10)]
-    (println gb)))
+  (start-server 46666))
